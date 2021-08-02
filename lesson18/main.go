40a2a8258f209f48be48d99dae54d1719a6c6558
latermonk <iback.china@gmail.com>
2021-08-02 11:33:12 +0800

    Update main.go

--- lesson18/main.go
+++ lesson18/main.go
@@ -9,12 +9,13 @@ import (
 func main() {
 	fmt.Println("==============Begining====================")
 
+	url := "https://www.baidu.com"
 
-
+	/* Sugar log 的使用方式*/
+/*
 	logger, _ := zap.NewProduction()
 	defer logger.Sync() // flushes buffer, if any
 	sugar := logger.Sugar()
-	url := "https://www.baidu.com"
 	sugar.Infow("failed to fetch URL",
 		// Structured context as loosely typed key-value pairs.
 		"url", url,
@@ -22,6 +23,20 @@ func main() {
 		"backoff", time.Second,
 	)
 	sugar.Infof("Failed to fetch URL: %s", url)
+*/
+
+
+	/* 生产环境 zap log 的使用方式*/
+	logger, _ := zap.NewProduction()
+	defer logger.Sync()
+	logger.Info("failed to fetch URL",
+		// Structured context as strongly typed Field values.
+		zap.String("url", url),
+		zap.Int("attempt", 3),
+		zap.Duration("backoff", time.Second),
+	)
+
+
 
 
 	fmt.Println("===========Ending =================")
