b94473a98dee331455cea0efc892dd0360f60a58
latermonk <iback.china@gmail.com>
2021-08-02 11:20:20 +0800

    up
    https://github.com/uber-go/zap

--- lesson18/go.mod
+++ lesson18/go.mod
@@ -1,3 +1,5 @@
 module lesson18
 
 go 1.16
+
+require go.uber.org/zap v1.18.1
--- lesson18/main.go
+++ lesson18/main.go
@@ -1,7 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"go.uber.org/zap"
+	"time"
+)
 
 func main() {
-	fmt.Println("Hello")
+	fmt.Println("==============Begining====================")
+
+
+
+	logger, _ := zap.NewProduction()
+	defer logger.Sync() // flushes buffer, if any
+	sugar := logger.Sugar()
+	url := "https://www.baidu.com"
+	sugar.Infow("failed to fetch URL",
+		// Structured context as loosely typed key-value pairs.
+		"url", url,
+		"attempt", 3,
+		"backoff", time.Second,
+	)
+	sugar.Infof("Failed to fetch URL: %s", url)
+
+
+	fmt.Println("===========Ending =================")
+
 }
